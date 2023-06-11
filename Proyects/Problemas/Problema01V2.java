450eca1011f3d015c8a6d33816ef73a2f2f43762
Jhossep <jhossep2718@gmail.com>
2023-06-10 21:39:22 -0500

    V2

---
+++ Proyects/Problemas/Problema01V2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Problema01V2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena = sc.nextLine().toLowerCase();
+        sc.close();
+        int numCual = 0;
+        int numComo = 0;
+
+        int i = 0;
+        while(i < cadena.length() - 3){
+            if( "como".equals(cadena.substring(i, i + 4)) ){
+                numComo += 1;
+            }
+            if( "cual".equals(cadena.substring(i, i + 4)) ){
+                numCual += 1;
+            }
+            i += 1;
+        }
+
+        if(numCual == numComo) {
+            System.out.println("Igual cantidad");
+        }else{
+            System.out.println("Distinta cantidad");
+        }
+    }
+}
