aada87a94c4b5d97cbc2aafa8db11e48c976d8c0
Jhossep <jhossep2718@gmail.com>
2023-06-15 03:39:28 -0500

    fix sc.close

--- Proyects/Metodos/SumaConMetodos.java
+++ Proyects/Metodos/SumaConMetodos.java
@@ -2,10 +2,12 @@ import java.util.*;
 class SumaConMetodos{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.close();
+        System.out.println("Ingrese primer sumando: ");
         int a = sc.nextInt();
+        System.out.println("Ingrese segundo sumando: ");
         int b = sc.nextInt();
         int d = Suma.sumaD(a, b);
-        System.out.println(d);
+        System.out.println("La suma es: " + d);
+        sc.close();
     }
 }
