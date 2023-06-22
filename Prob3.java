bfd57a59b549af28ac969ff6f1237323a2b0bfde
Jhossep <jhossep2718@gmail.com>
2023-06-21 22:57:42 -0500

    free photoshop

---
+++ Prob3.java
@@ -0,0 +1,17 @@
+package Proyects.If;
+import java.util.*;
+public class Prob3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantas canciones: ");
+        int cantidadCanciones = sc.nextInt();
+        int total = 0;
+        System.out.println("Cual es la duracion de las canciones: ");
+
+        for(int i = 0; i < cantidadCanciones; i++){
+            int duracionCancion = sc.nextInt();
+            total += duracionCancion;
+        }
+        System.out.println("El tiempo total es: " + total);
+    }
+}
---
+++ Proyects/EPISUNSA/TestImage.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import episunsa.*;
+class TestImage{
+    public static void main(String[] args){
+        SimpleImage img = new SimpleImage("stop.jpg");
+        SimpleImage back = new SimpleImage("leaves.jpg");
+        Scanner sc = new Scanner(System.in);
+
+        for(Pixel p : img){
+            int avg = ((p.getRed() + p.getGreen() + p.getBlue()) / 3); 
+            avg = (int)(avg * 1.5);
+            if(p.getRed() > avg){
+                int x = p.getX();
+                int y = p.getY();
+                Pixel pixel2 = back.getPixel(x, y);
+                p.setRed(pixel2.getRed());
+                p.setGreen(pixel2.getGreen());
+                p.setBlue(pixel2.getBlue());
+            }
+        }
+        img.print();
+    }
+}
