1c4690c8d1506334433f1879ec550c2e0dccf217
Jhossep <jhossep2718@gmail.com>
2023-07-17 01:15:52 -0500

    i found one bug with the method beside xD

---
+++ Proyects/Graphics_UNSA/AlLadoKaboom.java
@@ -0,0 +1,13 @@
+import graphics.*;
+class AlLadoKaboom {
+    public static void main(String[] args) {
+        Picture caballo = Picture.caballo();
+        Picture whiteCell = Picture.casilleroBlanco();
+        Picture blackCell = whiteCell.invertir();
+        Picture peon = Picture.peon();
+        //Graphics p = new Graphics(Picture.alfil().encima(blackCell).alLado(Picture.peon().superponer(whiteCell).encima(blackCell)).repetirV(4).repetirH(4));
+        //Graphics display = new Graphics(Picture.alfil().encima(blackCell).alLado(Picture.peon().superponer(whiteCell)).repetirV(4).repetirH(4));
+        Graphics a = new Graphics(caballo.alLado(peon).encima(whiteCell).repetirH(4));
+        a.print();
+    }
+}
