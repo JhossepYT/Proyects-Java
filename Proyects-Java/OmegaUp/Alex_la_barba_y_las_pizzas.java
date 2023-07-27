2a21cfcd055aeb9dc441de013cbf8a974bdc069c
Jhossep <jhossep2718@gmail.com>
2023-07-27 17:42:29 -0500

    more omegaup exercises

---
+++ Proyects-Java/OmegaUp/Alex_la_barba_y_las_pizzas.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Alex_la_barba_y_las_pyzzas{
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int dias = sc.nextInt();
+        int price = sc.nextInt();
+        int total = 0;
+
+        int i = 0;
+        while(i < dias){
+            int x = sc.nextInt();
+            total = total + x * price;
+            
+            i++;
+        }
+        System.out.println(total);
+    }
+}
---
+++ Proyects-Java/OmegaUp/Arreglo_Bidi2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Arreglo_Bidi2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] num = new int[n][n];
+        
+        for(int fila = 0; fila < n; fila++){
+            for(int columna = 0; columna < n; columna++){
+                num[fila][columna] = fila * columna;
+            }
+        }
+        
+        for(int fila = 0; fila < n; fila++){
+            for(int columna = 0; columna < n; columna++){
+                System.out.print(num[fila][columna]);
+            }
+            System.out.println();
+        }
+    }
+}
